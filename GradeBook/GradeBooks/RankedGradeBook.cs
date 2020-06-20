a49a9683b531a773c771c66b2a36b10b1c9bea29
Bogdan Gheorghe <bgheorghe@uwm.com>
2020-06-19 22:26:38 -0400

    Module completed

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -10,5 +10,33 @@ namespace GradeBook.GradeBooks
         {
             Type = Enums.GradeBookType.Ranked;
         }
+
+        public override char GetLetterGrade(double averageGrade)
+        {
+            int nbrStudents = Students.Count;
+
+            if (nbrStudents < 5)
+            {
+                throw new InvalidOperationException();
+            }
+
+            int studentsAboveGrade = 0;
+            foreach(Student student in Students)
+            {
+                if (student.AverageGrade > averageGrade)
+                    studentsAboveGrade++;
+            }
+
+            if (studentsAboveGrade >= nbrStudents * 0.8)
+                return 'A';
+            else if (studentsAboveGrade >= nbrStudents * 0.6)
+                return 'B';
+            else if (studentsAboveGrade >= nbrStudents * 0.4)
+                return 'C';
+            else if (studentsAboveGrade >= nbrStudents * 0.2)
+                return 'D';
+
+            return 'F';
+        }
     }
 }
